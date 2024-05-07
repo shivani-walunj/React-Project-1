66d41b8ef3eaf7199437fad1365074a109238dd7
shivani-walunj <shivaniiwalunj@gmail.com>
2024-05-07 19:41:00 +0530

    some data added

--- src/books.js
+++ src/books.js
@@ -1,6 +1,11 @@
 import img1 from "./images/book-1.jpg";
 import img2 from "./images/book-2.jpg";
 import img3 from "./images/book-3.jpg";
+import img4 from "./images/book-4.jpg";
+import img5 from "./images/book-5.jpg";
+import img6 from "./images/book-6.jpg";
+import img7 from "./images/book-7.jpg";
+
 export const books = [
   {
     author: "Jordan Moore",
@@ -19,5 +24,29 @@ export const books = [
     title: "Fairy Tale",
     img: img3,
     id: 3,
+  },
+  {
+    author: "Stephen King",
+    title: "Fairy Tale",
+    img: img4,
+    id: 4,
+  },
+  {
+    author: "Stephen King",
+    title: "Fairy Tale",
+    img: img5,
+    id: 5,
+  },
+  {
+    author: "Stephen King",
+    title: "Fairy Tale",
+    img: img6,
+    id: 6,
+  },
+  {
+    author: "Stephen King",
+    title: "Fairy Tale",
+    img: img7,
+    id: 7,
   },
 ];
